02f0211ba8e084ac3e2b2577f5df459132a64f05
Timmy Omolana <timmieomolanakiel@gmail.com>
2022-03-07 12:24:05 +0100

    Create Hello world contract

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: Unlicensed
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract HelloWorld {
+	string private greet_text; //state variable
+
+	constructor(string memory _gt) {
+	   console.log("Deploying Hello World contract greeting you: ", _gt);
+           greet_text = _gt;
+	}
+
+	getGreetText() public view returns (string memory) {
+	   return greet_text;
+	}
+
+	setGreetText(string memory new_greet) public {
+	   console.log("The hello world greeting text will change from %s to %s", greet_text, new_greet);
+	   greet_text = newGreet;
+	}
+}
+
